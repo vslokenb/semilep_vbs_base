a795152296ccb7f2994d7e71910fd24a907c5a30
david.buitrago <wdavidbuitragoceballos@gmail.com>
2025-07-29 14:53:17 +0200

    Agregué notebooks del tutorial y .gitignore

---
+++ Pocket/zmumutest/example_config.py
@@ -0,0 +1,101 @@
+from pocket_coffea.utils.configurator import Configurator
+from pocket_coffea.lib.cut_definition import Cut
+from pocket_coffea.lib.cut_functions import get_nObj_min, get_HLTsel, get_nPVgood, goldenJson, eventFlags
+from pocket_coffea.parameters.cuts import passthrough
+from pocket_coffea.parameters.histograms import *
+import workflow
+from workflow import ZmumuBaseProcessor
+from pocket_coffea.lib.weights.common import common_weights
+
+# Register custom modules in cloudpickle to propagate them to dask workers
+import cloudpickle
+import custom_cut_functions 
+cloudpickle.register_pickle_by_value(workflow)
+cloudpickle.register_pickle_by_value(custom_cut_functions)
+
+from custom_cut_functions import *
+import os
+localdir = os.path.dirname(os.path.abspath(__file__))
+
+# Loading default parameters
+from pocket_coffea.parameters import defaults
+default_parameters = defaults.get_default_parameters()
+defaults.register_configuration_dir("config_dir", localdir+"/params")
+
+parameters = defaults.merge_parameters_from_files(default_parameters,
+                                                  f"{localdir}/params/object_preselection.yaml",
+                                                  f"{localdir}/params/triggers.yaml",
+                                                  f"{localdir}/params/plotting.yaml",
+                                                  update=True)
+
+
+
+cfg = Configurator(
+    parameters = parameters,
+    datasets = {
+        "jsons": [f"{localdir}/datasets/DATA_SingleMuon.json",
+                  f"{localdir}/datasets/DYJetsToLL_M-50.json"
+                    ],
+        "filter" : {
+            "samples": ["DATA_SingleMuon",
+                        "DYJetsToLL"],
+            "samples_exclude" : [],
+            "year": ['2018']
+        }
+    },
+    
+
+    workflow = ZmumuBaseProcessor,
+
+    skim = [get_nPVgood(1), eventFlags, goldenJson, # basic skims
+            get_nObj_min(1, 18., "Muon"),
+            # Asking only SingleMuon triggers since we are only using SingleMuon PD data
+            get_HLTsel(primaryDatasets=["SingleMuon"])], 
+    
+    preselections = [dimuon_presel],
+    categories = {
+        "baseline": [passthrough],
+    },
+
+    weights_classes = common_weights,
+    
+    weights = {
+        "common": {
+            "inclusive": ["genWeight","lumi","XS",
+                          "pileup",
+                          "sf_mu_id","sf_mu_iso",
+                          ],
+            "bycategory" : {
+            }
+        },
+        "bysample": {
+        }
+    },
+
+    variations = {
+        "weights": {
+            "common": {
+                "inclusive": [  "pileup",
+                                "sf_mu_id", "sf_mu_iso"
+                              ],
+                "bycategory" : {
+                }
+            },
+        "bysample": {
+        }    
+        },
+    },
+
+    
+   variables = {
+        **muon_hists(coll="MuonGood", pos=0),
+        **count_hist(name="nElectronGood", coll="ElectronGood",bins=3, start=0, stop=3),
+        **count_hist(name="nMuonGood", coll="MuonGood",bins=3, start=0, stop=3),
+        **count_hist(name="nJets", coll="JetGood",bins=8, start=0, stop=8),
+        **count_hist(name="nBJets", coll="BJetGood",bins=8, start=0, stop=8),
+        **jet_hists(coll="JetGood", pos=0),
+        **jet_hists(coll="JetGood", pos=1),
+        "mll" : HistConf( [Axis(coll="ll", field="mass", bins=100, start=0, stop=200, label=r"$M_{\ell\ell}$ [GeV]")] ),
+    }
+)
+
---
+++ Pocket/zmumutest/workflow.py
@@ -0,0 +1,71 @@
+import awkward as ak
+
+from pocket_coffea.workflows.base import BaseProcessorABC
+from pocket_coffea.utils.configurator import Configurator
+from pocket_coffea.lib.hist_manager import Axis
+from pocket_coffea.lib.objects import (
+    jet_correction,
+    lepton_selection,
+    jet_selection,
+    btagging,
+    get_dilepton,
+)
+
+
+class ZmumuBaseProcessor(BaseProcessorABC):
+    def __init__(self, cfg: Configurator):
+        super().__init__(cfg)
+
+
+    def apply_object_preselection(self, variation):
+        '''
+        The ttHbb processor cleans
+          - Electrons
+          - Muons
+          - Jets -> JetGood
+          - BJet -> BJetGood
+
+        '''
+        # Include the supercluster pseudorapidity variable
+        electron_etaSC = self.events.Electron.eta + self.events.Electron.deltaEtaSC
+        self.events["Electron"] = ak.with_field(
+            self.events.Electron, electron_etaSC, "etaSC"
+        )
+        # Build masks for selection of muons, electrons, jets, fatjets
+        self.events["MuonGood"] = lepton_selection(
+            self.events, "Muon", self.params
+        )
+        self.events["ElectronGood"] = lepton_selection(
+            self.events, "Electron", self.params
+        )
+        leptons = ak.with_name(
+            ak.concatenate((self.events.MuonGood, self.events.ElectronGood), axis=1),
+            name='PtEtaPhiMCandidate',
+        )
+        self.events["LeptonGood"] = leptons[ak.argsort(leptons.pt, ascending=False)]
+
+        self.events["JetGood"], self.jetGoodMask = jet_selection(
+            self.events, "Jet", self.params, 
+            year=self._year, 
+            leptons_collection="LeptonGood"
+        )
+        self.events["BJetGood"] = btagging(
+            self.events["JetGood"], self.params.btagging.working_point[self._year], wp=self.params.object_preselection.Jet.btag.wp)
+
+        self.events["ll"] = get_dilepton(
+            self.events.ElectronGood, self.events.MuonGood
+        )
+
+    def count_objects(self, variation):
+        self.events["nMuonGood"] = ak.num(self.events.MuonGood)
+        self.events["nElectronGood"] = ak.num(self.events.ElectronGood)
+        self.events["nLeptonGood"] = (
+            self.events["nMuonGood"] + self.events["nElectronGood"]
+        )
+        self.events["nJetGood"] = ak.num(self.events.JetGood)
+        self.events["nBJetGood"] = ak.num(self.events.BJetGood)
+        # self.events["nfatjet"]   = ak.num(self.events.FatJetGood)
+
+    # Function that defines common variables employed in analyses and save them as attributes of `events`
+    def define_common_variables_before_presel(self, variation):
+        self.events["JetGood_Ht"] = ak.sum(abs(self.events.JetGood.pt), axis=1)
