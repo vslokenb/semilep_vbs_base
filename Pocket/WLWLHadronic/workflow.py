ee9d9a172420188d4ec5aa264b778926d4847b3b
W. David Buitrago Ceballos <123498473+wbuitrago@users.noreply.github.com>
2025-08-11 08:12:01 -0500

    Update workflow.py

--- Pocket/WLWLHadronic/workflow.py
+++ Pocket/WLWLHadronic/workflow.py
@@ -1,74 +1,104 @@
+# workflow.py
 import awkward as ak
-
+import numpy as np
 from pocket_coffea.workflows.base import BaseProcessorABC
 from pocket_coffea.utils.configurator import Configurator
-from pocket_coffea.lib.hist_manager import Axis
-from pocket_coffea.lib.objects import (
-    jet_correction,
-    lepton_selection,
-    jet_selection,
-    btagging,
-    get_dilepton,
-)
+from pocket_coffea.lib.objects import lepton_selection, jet_selection, btagging
 
+class VBSSemileptonicProcessor(BaseProcessorABC):
+    """
+    - Construye LeptonGood y JetGood (limpios de leptones)
+    - Identifica los VBS tagging jets como el par con mayor mjj
+    - Reconstruye el W hadrónico con dos jets no-VBS que minimizan |m-80.4|
+    - Calcula variables auxiliares para histogramas (mt, pt/eta, dR, etc.)
+    """
 
-class VBSWWBaseProcessor(BaseProcessorABC):
     def __init__(self, cfg: Configurator):
         super().__init__(cfg)
 
-
+    # 1) object-level preselection
     def apply_object_preselection(self, variation):
+        ev = self.events
 
-        # Include the supercluster pseudorapidity variable
-        electron_etaSC = self.events.Electron.eta + self.events.Electron.deltaEtaSC
-        self.events["Electron"] = ak.with_field(
-            self.events.Electron, electron_etaSC, "etaSC"
-        )
-        # Build masks for selection of muons, electrons, jets, fatjets
-        self.events["MuonGood"] = lepton_selection(
-            self.events, "Muon", self.params
-        )
-        self.events["ElectronGood"] = lepton_selection(
-            self.events, "Electron", self.params
-        )
+        # Electronss: etaSC for selection of ID 
+        ev["Electron", "etaSC"] = ev.Electron.eta + ev.Electron.deltaEtaSC
+
+        # Good Leptons (from YAML)
+        ev["MuonGood"]     = lepton_selection(ev, "Muon", self.params)
+        ev["ElectronGood"] = lepton_selection(ev, "Electron", self.params)
+
+        # Leptons (mu+e) and ordered in pt
         leptons = ak.with_name(
-            ak.concatenate((self.events.MuonGood, self.events.ElectronGood), axis=1),
-            name='PtEtaPhiMCandidate',
+            ak.concatenate([ev.MuonGood, ev.ElectronGood], axis=1),
+            "PtEtaPhiMCandidate",
         )
-        self.events["LeptonGood"] = leptons[ak.argsort(leptons.pt, ascending=False)]
+        ev["LeptonGood"] = leptons[ak.argsort(leptons.pt, ascending=False)]
 
-        self.events["ll"] = get_dilepton(
-            self.events.ElectronGood, self.events.MuonGood
+        ev["JetGood"], _ = jet_selection(ev, "Jet", self.params, "LeptonGood")
+        ev["JetGood", "idx"] = ak.local_index(ev.JetGood, axis=1)
+
+        # b-tagging (for veto)
+        ev["BJetGood"] = btagging(
+            ev.JetGood,
+            self.params.btagging.working_point[self._year],
+            wp=self.params.object_preselection.Jet.btag.wp,
         )
 
-        self.events["JetGood"], self.jetGoodMask = jet_selection(
-            self.events, "Jet", self.params, "LeptonGood"
+        # ------------- VBS tagging jets -------------
+        has4j = ak.num(ev.JetGood) >= 4
+
+        jj = ak.combinations(ev.JetGood, 2, fields=["jet1", "jet2"])
+        jj["mass"] = (jj.jet1 + jj.jet2).mass
+
+        idx_vbs = ak.argmax(jj.mass, axis=1, keepdims=True)
+        ev["vbsjets"] = ak.mask(jj[idx_vbs], has4j)
+
+        v1 = ak.firsts(ev.vbsjets.jet1)
+        v2 = ak.firsts(ev.vbsjets.jet2)
+
+        # deta and dR between jets
+        ev["vbsjets", "delta_eta"] = np.abs(v1.eta - v2.eta)
+        ev["vbs_dR"] = ak.fill_none(v1.delta_r(v2), np.nan)
+
+        # ------------- W hadronic (resolved) -------------
+        # index of the VBS jets in the colection JetGood
+        vbs_i = ak.fill_none(getattr(v1, "idx", None), -1)
+        vbs_j = ak.fill_none(getattr(v2, "idx", None), -1)
+
+        nonvbs_mask = (ev.JetGood.idx != vbs_i) & (ev.JetGood.idx != vbs_j)
+        jets_nonvbs = ev.JetGood[nonvbs_mask]
+
+        pairs_w = ak.combinations(jets_nonvbs, 2, fields=["jet1", "jet2"])
+        pairs_w["mass"] = (pairs_w.jet1 + pairs_w.jet2).mass
+
+        target_mw = 80.4
+        best_w_idx = ak.argmin(np.abs(pairs_w.mass - target_mw), axis=1, keepdims=True)
+
+        ev["w_had_jets"] = ak.mask(pairs_w[best_w_idx], has4j)
+        ev["w_had_jets", "mass"] = (ev.w_had_jets.jet1 + ev.w_had_jets.jet2).mass
+
+        # dR between the two jets of the W_had
+        wj1 = ak.firsts(ev.w_had_jets.jet1)
+        wj2 = ak.firsts(ev.w_had_jets.jet2)
+        ev["w_had_dR"] = ak.fill_none(wj1.delta_r(wj2), np.nan)
+
+        # ------------- W leptonic(mT) -------------
+        lead_lep = ak.firsts(ev.LeptonGood)
+        ev["mt_w_leptonic"] = np.sqrt(
+            2.0 * lead_lep.pt * ev.MET.pt * (1.0 - np.cos(lead_lep.delta_phi(ev.MET)))
         )
-        self.events["BJetGood"] = btagging(
-            self.events["JetGood"], self.params.btagging.working_point[self._year], wp=self.params.object_preselection.Jet.btag.wp)
 
-        jj_pairs = ak.combinations(self.events.JetGood, 2, fields=["jet1", "jet2"])
-        jj_pairs["mass"] = (jj_pairs.jet1 + jj_pairs.jet2).mass
-        idx_vbs_jets = ak.argmax(jj_pairs.mass, axis=1, keepdims=True)
-        vbs_jets_pair = jj_pairs[idx_vbs_jets]
-
-        self.events["vbsjets"] = vbs_jets_pair
-        self.events["vbsjets"] = ak.with_field(self.events.vbsjets, abs(self.events.vbsjets.jet1.eta - self.events.vbsjets.jet2.eta), where="delta_eta")
-
-        self.events["vbsjet_1"] = self.events.vbsjets.jet1
-        self.events["vbsjet_2"] = self.events.vbsjets.jet2
-        
+        # ------------- helpers for plots -------------
+        jets_sorted = ev.JetGood[ak.argsort(ev.JetGood.pt, ascending=False)]
+        ev["jet1_pt"]  = ak.firsts(getattr(jets_sorted[:, 0:1], "pt", None))
+        ev["jet2_pt"]  = ak.firsts(getattr(jets_sorted[:, 1:2], "pt", None))
+        ev["jet1_eta"] = ak.firsts(getattr(jets_sorted[:, 0:1], "eta", None))
+        ev["jet2_eta"] = ak.firsts(getattr(jets_sorted[:, 1:2], "eta", None))
 
     def count_objects(self, variation):
-        self.events["nMuonGood"] = ak.num(self.events.MuonGood)
-        self.events["nElectronGood"] = ak.num(self.events.ElectronGood)
-        self.events["nLeptonGood"] = (
-            self.events["nMuonGood"] + self.events["nElectronGood"]
-        )
-        self.events["nJetGood"] = ak.num(self.events.JetGood)
-        self.events["nBJetGood"] = ak.num(self.events.BJetGood)
-        # self.events["nfatjet"]   = ak.num(self.events.FatJetGood)
-
-    def define_common_variables_before_presel(self, variation):
-        pass
-       # self.events["JetGood_Ht"] = ak.sum(abs(self.events.JetGood.pt), axis=1)
+        ev = self.events
+        ev["nMuonGood"]     = ak.num(ev.MuonGood)
+        ev["nElectronGood"] = ak.num(ev.ElectronGood)
+        ev["nLeptonGood"]   = ev.nMuonGood + ev.nElectronGood
+        ev["nJetGood"]      = ak.num(ev.JetGood)
+        ev["nBJetGood"]     = ak.num(ev.BJetGood)
